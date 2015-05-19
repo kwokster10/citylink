edecb870c418185948b5f0e0173d57f28798dad0
kwokster10 <kwokster10@hotmail.com>
2015-05-18 21:36:04 -0400

    payees

--- app/controllers/payees_controller.rb
+++ app/controllers/payees_controller.rb
@@ -1,19 +1,34 @@
 class PayeesController < ApplicationController
-  def new
+  def index 
+  	@payees = Payee.all
+  end
 
+  def new
   end
 
   def create
   	@payee = Payee.create(payee_params)
+  	@payee[:user_id] = session[:user_id]
+  	@payee.save
   	redirect_to activities_payment_path
   end
 
   def edit 
+  	@payee = Payee.find(params[:id])
   end
 
   def update
+  	@payee = Payee.find(params[:id])
+  	@payee.update(payee_params)
+  	redirect_to activities_path
   end
-  
+
+  def destroy 
+  	@payee = Payee.find(params[:id])
+  	@payee.destroy
+  	redirect_to activities_path
+  end
+
   private 
   def payee_params
   	params.require(:user).permit(:name, :email)
