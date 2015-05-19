77bdad53e0dcedc03d2bbfc24fe4f127bb4a6e57
Evan Griffiths <griffithse@gmail.com>
2015-05-18 20:03:33 -0400

    broke stuff

---
+++ app/controllers/payees_controller.rb
@@ -0,0 +1,11 @@
+class PayeesController < ApplicationController
+  def
+end
+
+       #      payees POST   /payees(.:format)             payees#create
+       #  new_payees GET    /payees/new(.:format)         payees#new
+       # edit_payees GET    /payees/edit(.:format)        payees#edit
+       #             GET    /payees(.:format)             payees#show
+       #             PATCH  /payees(.:format)             payees#update
+       #             PUT    /payees(.:format)             payees#update
+       #             DELETE /payees(.:format)             payees#destroy
