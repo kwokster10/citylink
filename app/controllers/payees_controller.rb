0a12156dd1efa2fa14afb92cc9909db23ad27530
kwokster10 <kwokster10@hotmail.com>
2015-05-18 21:24:29 -0400

    payees

--- app/controllers/payees_controller.rb
+++ app/controllers/payees_controller.rb
@@ -1,5 +1,24 @@
 class PayeesController < ApplicationController
-  def
+  def new
+
+  end
+
+  def create
+  	@payee = Payee.create(payee_params)
+  	redirect_to activities_payment_path
+  end
+
+  def edit 
+  end
+
+  def update
+  end
+  
+  private 
+  def payee_params
+  	params.require(:user).permit(:name, :email)
+  end
+
 end
 
        #      payees POST   /payees(.:format)             payees#create
